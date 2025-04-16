fae2f89d74e660f263c892b57b7cd4090fb4810b
Dominik Borbuliak <borbuliak.dominik@gmail.com>
2025-04-16 23:18:18 +0200

    feat: Add use static files

--- PV260.Project/PV260.Project.Server/Program.cs
+++ PV260.Project/PV260.Project.Server/Program.cs
@@ -12,6 +12,7 @@ WebApplication app = builder.Build();
 
 app.UseDefaultFiles();
 app.MapStaticAssets();
+app.UseStaticFiles();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
