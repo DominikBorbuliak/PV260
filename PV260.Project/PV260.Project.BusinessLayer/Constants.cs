226fea0f49e097355476237e395d68047036d322
Dominik Borbuliak <borbuliak.dominik@gmail.com>
2025-04-21 15:31:26 +0200

    refactor: Altered unauthorized error message

--- PV260.Project/PV260.Project.BusinessLayer/Constants.cs
+++ PV260.Project/PV260.Project.BusinessLayer/Constants.cs
@@ -5,7 +5,7 @@ public static class Constants
     public static class Error
     {
         public const string NotFoundFormat = "{0} with provided {1} not found.";
-        public const string Unauthorized = "User is unauthorized. Please, try again later.";
+        public const string Unauthorized = "User is unauthorized. Please, login.";
         public const string Unexpected = "Unexpected error occured. Please, try again later.";
     }
 }
