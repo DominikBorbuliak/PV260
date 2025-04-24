116840eb4c3703d18cb1f056f05ad2e20469da20
Soňa Nemečkayová <sona.nemeckayova@gmail.com>
2025-04-24 20:16:53 +0200

    feat: Add DI

--- PV260.Project/PV260.Project.Server/Extensions/WebApplicationBuilderExtensions.cs
+++ PV260.Project/PV260.Project.Server/Extensions/WebApplicationBuilderExtensions.cs
@@ -88,9 +88,12 @@ public static class WebApplicationBuilderExtensions
     {
         _ = builder.Services.AddScoped<IArkFundsApiRepository, ArkFundsApiRepository>();
         _ = builder.Services.AddScoped<IUserRepository, UserRepository>();
+        _ = builder.Services.AddScoped<IReportRepository, ReportRepository>();
+
 
         _ = builder.Services.AddScoped<IUserService, UserService>();
         _ = builder.Services.AddScoped<IEmailSender, EmailSender>();
+        _ = builder.Services.AddScoped<IReportService, ReportService>();
 
         return builder;
     }
