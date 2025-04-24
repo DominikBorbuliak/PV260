6d4bdfa3dd64fc899940233a1e17f86489563296
Soňa Nemečkayová <sona.nemeckayova@gmail.com>
2025-04-24 20:11:19 +0200

    feat: Add report related models

---
+++ PV260.Project/PV260.Project.Domain/Models/Report.cs
@@ -0,0 +1,12 @@
+﻿namespace PV260.Project.Domain.Models;
+
+public class Report
+{
+    public Guid Id { get; set; }
+
+    public ReportDiff? Diff { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public IList<ArkFundsHolding> Holdings { get; set; } = new List<ArkFundsHolding>();
+}
---
+++ PV260.Project/PV260.Project.Domain/Models/ReportDiff.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PV260.Project.Domain.Models;
+
+public class ReportDiff
+{
+    public List<HoldingChange> Changes { get; set; } = new();
+}
