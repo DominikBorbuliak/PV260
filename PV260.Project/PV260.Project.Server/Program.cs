b3fc3ca8d49b9aef83367a5c2a70554106d33e88
mario <mario.straus@databy.io>
2025-05-04 21:58:21 +0200

    ref: SeedData are now as an app extension

--- PV260.Project/PV260.Project.Server/Program.cs
+++ PV260.Project/PV260.Project.Server/Program.cs
@@ -1,4 +1,3 @@
-using PV260.Project.Server.Data;
 using PV260.Project.Server.Extensions;
 
 try
@@ -12,24 +11,11 @@ try
         .ConfigureControllers()
         .Build();
     
-    app.ApplyMigrations();
-
-    using (IServiceScope scope = app.Services.CreateScope())
-    {
-        IServiceProvider services = scope.ServiceProvider;
-        try
-        {
-            await SeedData.EnsureRolesAsync(services);
-            await SeedData.EnsureAdminUserAsync(services);
-            await SeedData.EnsureSeedUsersAsync(services);
-        }
-        catch (Exception ex)
-        {
-            ILogger<Program> logger = services.GetRequiredService<ILogger<Program>>();
-            logger.LogError(ex, "Error while seeding database");
-        }
-    }
-
+    app.ApplyMigrations()
+        .SeedRoles()
+        .SeedAdminUser()
+        .SeedUser();
+    
     app.ConfigureApplication().Run();
 }
 catch (Exception exception)
