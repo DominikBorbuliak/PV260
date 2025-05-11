e39eb3ec8a012ba72366a4cc6d5259f2b53653a7
Dominik Borbuliak <borbuliak.dominik@gmail.com>
2025-05-11 19:27:46 +0200

    feat: Added unit of work

--- PV260.Project/PV260.Project.Domain/Interfaces/Infrastructure/Persistence/IReportRepository.cs
+++ PV260.Project/PV260.Project.Domain/Interfaces/Infrastructure/Persistence/IReportRepository.cs
@@ -5,6 +5,8 @@ namespace PV260.Project.Domain.Interfaces.Infrastructure.Persistence;
 public interface IReportRepository
 {
     Task SaveReportAsync(IList<ArkFundsHolding> holdings, ReportDiff diff);
+
     Task<Report?> GetLatestReportAsync();
+
     Task<Report?> GetClosestPreviousReportAsync(DateTime date);
 }
--- PV260.Project/PV260.Project.Server/Extensions/WebApplicationBuilderExtensions.cs
+++ PV260.Project/PV260.Project.Server/Extensions/WebApplicationBuilderExtensions.cs
@@ -94,6 +94,8 @@ public static class WebApplicationBuilderExtensions
         _ = builder.Services.AddScoped<IUserRepository, UserRepository>();
         _ = builder.Services.AddScoped<IReportRepository, ReportRepository>();
 
+        _ = builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
         _ = builder.Services.AddScoped<IUserService, UserService>();
         _ = builder.Services.AddScoped<IEmailSender, EmailSender>();
         _ = builder.Services.AddScoped<IReportService, ReportService>();
