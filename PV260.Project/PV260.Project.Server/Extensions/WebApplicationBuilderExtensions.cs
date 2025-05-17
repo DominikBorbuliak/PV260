04aa64e915c16f078c7555740d352fd3bcb91ea2
Dominik Borbuliak <borbuliak.dominik@gmail.com>
2025-05-17 15:10:04 +0200

    feat: Added logging

--- PV260.Project/PV260.Project.Server/Extensions/WebApplicationBuilderExtensions.cs
+++ PV260.Project/PV260.Project.Server/Extensions/WebApplicationBuilderExtensions.cs
@@ -18,6 +18,7 @@ using PV260.Project.Infrastructure.Persistence.Models;
 using PV260.Project.Infrastructure.Persistence.Repositories;
 using Quartz;
 using Quartz.AspNetCore;
+using Serilog;
 
 namespace PV260.Project.Server.Extensions;
 
@@ -32,7 +33,6 @@ public static class WebApplicationBuilderExtensions
         {
             _ = options
                 .UseSqlite(sqliteConnectionString)
-                .LogTo(a => Console.WriteLine(a), LogLevel.Debug)
                 .EnableSensitiveDataLogging(true)
                 .EnableDetailedErrors()
                 .UseLazyLoadingProxies();
@@ -151,6 +151,23 @@ public static class WebApplicationBuilderExtensions
         });
 
         _ = builder.Services.AddQuartzServer(opt => opt.WaitForJobsToComplete = true);
+
+        return builder;
+    }
+
+    public static WebApplicationBuilder ConfigureLogger(this WebApplicationBuilder builder)
+    {
+        Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.File("logs/Log.txt", rollingInterval: RollingInterval.Day)
+                .WriteTo.Console()
+                .CreateLogger();
+
+        _ = builder.Host.UseSerilog();
+
+        _ = builder.Logging.ClearProviders();
+        _ = builder.Logging.AddConsole();
+        _ = builder.Logging.AddDebug();
 
         return builder;
     }
